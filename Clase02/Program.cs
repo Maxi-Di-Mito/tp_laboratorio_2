19310ab0f8568ab7fcce4c35c46d6f94d0fdda2e
unknown <max90.dm@gmail.com>
2016-08-18 20:36:40 -0300

    Initial commit

---
+++ Clase02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "Hola mundo";
+
+            Sello.color = ConsoleColor.Cyan;
+            Sello.ImprimirColor();
+
+            Console.WriteLine(Sello.Imprimir());            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase02/Sello.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase02
+{
+    class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        static string ArmarFormatoMensaje()
+        {
+            string bar = new String('*', Sello.mensaje.Length + 2 );
+            string cadena = bar + "\n" + "*" + Sello.mensaje + "*\n" + bar;
+            return cadena;
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+
+        public static void ImprimirColor()
+        {
+            ConsoleColor colorAnterior = Console.ForegroundColor;
+            Console.ForegroundColor = Sello.color;
+            Console.WriteLine(Sello.mensaje);
+            Console.ForegroundColor = colorAnterior;
+        }
+
+        public static string Imprimir()
+        {
+            return Sello.ArmarFormatoMensaje();
+        }
+
+    }
+}
