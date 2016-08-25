690b65184e18e305ed9c090860f1c6297a5dad5c
maurosaquino <mauro.s.aquino@gmail.com>
2016-08-24 22:12:11 -0300

    Final clase 24-08

---
+++ Clase03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rec = new Rectangulo(5,12);
+            
+            Triangulo tri = new Triangulo(5,15);
+
+            //Rectangulo.Dibujar(rec);
+
+            Triangulo.Dibujar(tri);
+
+            Console.ReadKey();
+        }
+    }
+}
