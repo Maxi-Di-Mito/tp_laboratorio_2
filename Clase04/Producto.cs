5a4a4d3a4117cc82c757ebacf15700447805138b
unknown <max90.dm@gmail.com>
2016-08-25 21:01:36 -0300

    Clase04

---
+++ Clase04/Producto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase04
+{
+    class Producto
+    {        
+        private string codBarra;
+        private double precio;
+        private string nombre;
+
+        #region Constructores
+
+        /// <summary>
+        ///     Genera un nuevo producto
+        /// </summary>
+        /// <param name="precio">Precio de venta del producto</param>
+        /// <param name="codigoDeBarras">Codigo de barras del producto</param>
+        /// <param name="nombre">Nombre del producto</param>
+        public Producto(double precio, string codigoDeBarras , string nombre)
+        {
+            this.precio = precio;
+            this.codBarra = codigoDeBarras;
+            this.nombre = nombre;
+        }
+        /// <summary>
+        ///     Genera un nuevo objeto Producto
+        /// </summary>
+        /// <param name="precio">Precio de venta del producto</param>
+        /// <param name="codigoDeBarras">Codigo de barras del producto</param>
+        public Producto(double precio, string codigoDeBarras):this(precio)//reutilizacion e constructor
+        {   
+            this.codBarra = codigoDeBarras;            
+        }
+        /// <summary>
+        ///     Genera un nuevo objeto Producto
+        /// </summary>
+        /// <param name="precio">Precio de venta del producto</param>
+        public Producto(double precio):this()
+        {            
+            this.precio = precio;            
+        }
+        /// <summary>
+        ///     Genera un nuevo objeto Producto
+        /// </summary>
+        public Producto()
+        {
+            this.precio = 0.0;
+            this.codBarra = "sinCodigo";
+            this.nombre = "sinNombre";
+        }
+        
+
+        #endregion
+
+        /// <summary>
+        ///     Imprime la representacion en string del objeto Producto
+        /// </summary>
+        public void Mostrar()
+        {
+            Console.WriteLine("CODIGO: "+this.codBarra+"\nNOMBRE: "+this.nombre+"\nPRECIO: "+this.precio);
+        }
+
+    }
+}
---
+++ Clase04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto p1 = new Producto(12.9, "asd123", "Cepillo de dientes");
+            Producto p2 = new Producto(12.9, "qwe789");
+            Producto p3 = new Producto(12.9);
+            Producto p4 = new Producto();
+
+            Producto p5 = new Producto(codigoDeBarras:"fgh345",precio: 10.3, nombre:"Mate");
+
+            p1.Mostrar();
+            p2.Mostrar();
+            p3.Mostrar();
+            p4.Mostrar();
+            p5.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
