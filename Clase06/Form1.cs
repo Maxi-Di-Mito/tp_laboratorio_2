ced90f080376f7d680edacbf833f4a070488e70d
unknown <max90.dm@gmail.com>
2016-09-01 21:57:30 -0300

    TP1 first commit

---
+++ Clase06/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase06
+{
+    public partial class Form1 : Form
+    {
+        private int contadorDeClicks;
+        private Color[] colores = { Color.Purple, Color.Red, Color.Yellow, Color.Green };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.contadorDeClicks = 0;
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            this.lblLabel.Text = "Hola mundo";
+            this.Text = "Bienvenidos " + DateTime.Now;
+            this.contadorDeClicks++;
+            if (contadorDeClicks >= colores.Length)
+            {
+                contadorDeClicks = 0;
+            }
+            this.BackColor = colores[contadorDeClicks];            
+        }
+
+        
+
+        
+    }
+}
---
+++ TPs/DiMito.Maximiliano.2C/TP1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1
+{
+    public partial class frmCalculadora : Form
+    {
+        private string[] operadores;
+
+        public frmCalculadora()
+        {
+            InitializeComponent();
+            this.operadores = Calculadora.operadores;
+            this.cmbOperacion.Items.AddRange(this.operadores);   
+            this.cmbOperacion.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            Numero n1 = new Numero(this.txtNumero1.Text);
+            Numero n2 = new Numero(this.txtNumero2.Text);
+            this.lblResultado.Text = Calculadora.Operar(n1, n2, this.cmbOperacion.Text).ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+        }
+
+
+
+    }
+}
