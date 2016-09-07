dab1b1ff4d2d6b46d7070eca4a011c3bc5cd77b7
unknown <Max90.d@gmail.com>
2016-09-07 20:30:22 -0300

    Recreo 2016-09-07

---
+++ Clase07/Paleta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase07
+{
+    class Paleta
+    {
+        private Tempera[] _colores;
+        private sbyte _cantidadMaximaColores;
+
+        private Paleta() : this(4) { }        
+
+        private Paleta(sbyte cant)
+        {
+            this._cantidadMaximaColores = cant;
+            this._colores = new Tempera[cant];
+        }
+
+        public static implicit operator Paleta(sbyte cant)
+        {
+            return new Paleta(cant);
+        }
+
+        private static string Mostrar(Paleta p)
+        {
+            string ret = "CantidadMaximaDeColores : " + p._cantidadMaximaColores + "\n";
+            foreach (Tempera t in p._colores)
+            {
+                ret += t;
+            }
+            return ret;
+        }        
+
+        public static implicit operator String(Paleta p)
+        {
+            return Paleta.Mostrar(p);
+        }
+
+
+        /*public static void operator +(Paleta p, Tempera t)
+        {
+            
+        }*/
+
+
+
+        
+    }
+}
---
+++ Clase07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] colores = {ConsoleColor.Black, ConsoleColor.Cyan, ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.DarkMagenta};
+            string[] nombres = {"Hasbro","Parker","Chevrolet","Parker","Lumilagro"};
+            sbyte[] cantidades = { 1,2,3,4,5};
+
+            Tempera[] temperas = new Tempera[5];
+            
+            for (int i = 0; i < temperas.Length; i++)
+            {
+                temperas[i] = new Tempera(colores[i],nombres[i],cantidades[i]);
+            }
+
+            foreach (Tempera t in temperas)
+            {
+                Console.WriteLine(t);
+            }
+
+            Paleta p = 8;
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
