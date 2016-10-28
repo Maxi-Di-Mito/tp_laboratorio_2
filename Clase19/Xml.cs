f356679452527db053b63ca7c37efb1f36336428
unknown <max90.dm@gmail.com>
2016-10-27 22:02:55 -0300

    Clase 19

---
+++ Clase19/Xml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase19
+{
+    class Xml:IArchivo<Object>
+    {
+        public bool guardar(string archivo, object datos)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+
+        public bool leer(string archivo, object Datos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
