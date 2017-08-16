ca09222ae521f17a6420f1fd95c785fae7bd3602
alumno <alumno>
2017-08-16 20:02:14 -0300

    Clase 1 ejercicios

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pisos = 5;
+            int contador = 1;
+            string line = "*";
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out pisos));
+            
+            line = line.Insert(0,new String(' ',pisos-1));
+            
+            while (contador <= pisos)
+            {               
+                Console.WriteLine(line);                
+                line = line.Substring(1, line.Length-1) + "**";
+                contador++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pisos = 5;
+            int contador = 1;
+            string line = "";
+            string input;
+            do
+            {
+                 input = Console.ReadLine();
+            }while(!Int32.TryParse(input, out pisos));
+                        
+
+            while (contador <= pisos)
+            {
+                line += "*";
+                Console.WriteLine(line);
+                contador++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
