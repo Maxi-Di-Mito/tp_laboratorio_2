d24bae55b86fd50684a114c92e1661bb31c4e773
alumno <alumno>
2017-08-16 21:58:42 -0300

    Ejercicio 3 Clase 1

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int numero;
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out numero));
+
+            Console.WriteLine("Los numero primos menores a su numero son:");
+            for (int i = 1; i < numero; i++)
+            {
+                if (esPrimo(i))
+                    Console.WriteLine(i);
+            }
+            
+            Console.ReadKey();
+        }
+
+
+        static bool esPrimo(int numero)
+        {
+            if (numero == 1)
+                return true;
+            else
+            {
+                for (int i = numero-1; i > 1; i--)
+                {
+                    if (numero % i == 0)
+                        return false;
+                }
+                return true;
+            }
+                
+        }
+
+    }
+}
