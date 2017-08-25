1aa2917af75df85c1fbbc1d15fa5cf05a8df24b3
alumno <alumno>
2017-08-24 21:58:53 -0300

    Clase 4, guia tp01

---
+++ EjercicioClase4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador s = new Sumador();
+
+            s.Sumar(2, 4);
+
+            int c = (int)s;
+
+
+        }
+    }
+}
---
+++ EjercicioClase4/Sumador.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase4
+{
+    class Sumador
+    {
+        private int cantDeSumas;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Sumador()
+        {
+            this.cantDeSumas = 0;
+        }
+
+        /// <summary>
+        /// Suma dos longs en un long
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public long Sumar(long a, long b)
+        {
+            this.cantDeSumas++;
+            return a + b;
+        }
+
+        /// <summary>
+        /// Concatena(suma) dos strings
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string Sumar(string a, string b)
+        {
+            this.cantDeSumas++;
+            return a + b;
+        }
+
+        /// <summary>
+        /// Casteo de Sumador a int
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantDeSumas;
+        }
+
+        /// <summary>
+        /// Devuelve la suma de las dos cantidades de sumas
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long operator +(Sumador a, Sumador b)
+        {
+            return a.cantDeSumas + b.cantDeSumas;
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(Sumador a, Sumador b)
+        {
+            return !(a != b);
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(Sumador a, Sumador b)
+        {
+            return a.cantDeSumas == b.cantDeSumas;
+        }
+
+    }
+}
