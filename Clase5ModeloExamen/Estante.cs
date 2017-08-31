f4e30759af24abbc0a7840f995fce43bedeac256
alumno <alumno>
2017-08-30 22:16:47 -0300

    Ejercicio examen clase5

---
+++ Clase5ModeloExamen/Estante.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5ModeloExamen
+{
+    class Estante
+    {
+        private int _max;
+        private int _ubicacion;
+        private Producto[] _productos;
+
+
+        private Estante(int cant)
+        {
+            this._productos = new Producto[cant];
+        }
+
+        public Estante(int cant, int ubicacion)
+            : this(cant)
+        {
+            this._ubicacion = ubicacion;
+            this._max = cant;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this._productos;
+        }
+
+        public static String MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Ubicacion: " + e._ubicacion);
+            sb.AppendLine("Capacidad Maxima: " + e._max);
+
+            foreach(Producto p in e._productos) {
+                sb.AppendLine("Producto: " + Producto.MostrarProducto(p));
+            }
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estante estante, Producto prod)
+        {
+            foreach (Producto p in estante._productos)
+            {
+                if (p == prod)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante estante, Producto prod)
+        {
+            return !(estante == prod);
+        }
+
+        public static bool operator +(Estante estante, Producto prod)
+        {
+            return (estante._productos.Length < estante._max) && (estante != prod);///TODO QUE ONDA esto?
+        }
+
+        public static Estante operator -(Estante estante, Producto prod)
+        {
+            Estante nuevoEstante = new Estante(estante._max, estante._ubicacion);
+            
+            foreach (Producto p in estante._productos)
+            {
+                if (p != prod)
+                {
+                    nuevoEstante._pro//TODO hacer metodo y consultar el +;
+                }
+            }
+
+            return nuevoEstante;
+        }
+
+    }
+}
---
+++ Clase5ModeloExamen/Producto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5ModeloExamen
+{
+    class Producto
+    {
+        private String _marca;
+        private float _precio;
+        private String _codigoBarra;
+
+        public Producto()
+        {
+
+        }
+
+
+        public String GetMarca(){
+            return this._marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this._precio;
+        }
+
+        public static String MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Marca: " + p._marca);
+            sb.AppendLine("Precio: " + p._precio);
+            sb.AppendLine("Codigo: " + p._codigoBarra);
+            
+            return sb.ToString();
+        }
+
+
+        public static bool operator ==(Producto a, Producto b)
+        {
+            return a._marca == b._marca && a._codigoBarra == b._codigoBarra;
+        }
+
+        public static bool operator !=(Producto a, Producto b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator ==(Producto prod, String marca)
+        {
+            return prod._marca == marca;
+        }
+        public static bool operator !=(Producto prod, String marca)
+        {
+            return !(prod == marca);
+        }
+
+
+        public static explicit operator String(Producto prod)
+        {
+            return prod._codigoBarra;
+        }
+
+
+
+    }
+}
