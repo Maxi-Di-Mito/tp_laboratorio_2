5d491f0dd2da614040835b980e41c7a0083eb62f
alumno <alumno>
2017-10-25 22:06:25 -0300

    Clase Jornada inital commit

---
+++ TP03/DiMito.Maximiliano.2c.TP3/TP3/Jornada.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    class Jornada
+    {
+        private List<Alumno> _alumnos;
+        private EClases _clase;
+        private Profesor _instructor;
+
+
+        public List<Alumno> Alumnos
+        {
+            get
+            {
+                return this._alumnos;
+            }
+            set
+            {
+                this._alumnos = value;
+            }
+        }
+
+        public EClases Clase
+        {
+            get
+            {
+                return this._clase;
+            }
+            set
+            {
+                this._clase = value;
+            }
+        }
+
+        public Profesor Instructor
+        {
+            get
+            {
+                return this._instructor;
+            }
+            set
+            {
+                this._instructor = value;
+            }
+        }
+
+        private Jornada()
+        {
+            this._alumnos = new List<Alumno>();
+        }
+
+
+        public Jornada(EClases clase, Profesor instructor)
+        {
+            this._clase = clase;
+            this._instructor = instructor;
+        }
+
+
+        public String Leer()
+        {
+            return "";
+        }
+
+        public String ToString()
+        {
+            return "";
+        }
+
+        /* OPERATORS*/
+
+    }
+}
