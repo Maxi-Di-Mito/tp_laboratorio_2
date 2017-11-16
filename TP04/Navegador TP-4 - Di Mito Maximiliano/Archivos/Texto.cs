848d0d7ae775307d678052cf1a998f91f8c07c0b
alumno <alumno>
2017-11-16 20:34:03 -0300

    TP04

---
+++ TP04/Navegador TP-4 - Di Mito Maximiliano/Archivos/Texto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string path;
+
+        public Texto(string archivo)
+        {
+            this.path = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(this.path,true);
+                writer.WriteLine(datos);
+
+                writer.Flush();
+                writer.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                return false;
+            }
+            
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            try
+            {
+                List<String> leidos = new List<string>();
+                StreamReader reader = new StreamReader(this.path);
+                while (!reader.EndOfStream)
+                    leidos.Add(reader.ReadLine());
+
+                reader.Close();
+                datos = leidos;
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                datos = null;
+                return false;
+            }
+            
+        }
+    }
+}
